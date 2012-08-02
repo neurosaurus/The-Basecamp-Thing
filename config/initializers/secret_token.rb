58f54c2dc4798bdde6daf7d3a7fa596f7162d937
Tripta Gupta + Ivan Stroganov <apprentice+tg+is@devbootcamp.com>
2012-08-02 12:25:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bcclone::Application.config.secret_token = '996180ef1156e847f49e2ec82317daad6bbee426421d026ed9e47c539e6025fb3b556c653a45ca7d81a2e9e09bc69ab600d3510d6394ba027bf02099b8906d00'
